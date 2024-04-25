525b086fb35d854b8e30ca88b930e5d9214fa9a3
Erika-CV <erikacamposdeveloper@gmail.com>
2024-04-25 20:02:04 -0300

    agregando formulario y ubicacion

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+//icono hamburguesa
+
 const toggle = document.querySelector(".toggle");
 const navMenu = document.querySelector(".nav-menu");
 
 toggle.addEventListener("click",() =>{
     navMenu.classList.toggle("nav-menu_visible");
 });
+
+
+
+
+// validacion formulario
+
+var validacion = () => {
+    let dato1 = document.forms[0].elements[0].value 
+    let dato2 = document.forms[0].elements[1].value 
+    let dato3 = document.forms[0].elements[2].value
+    let dato4 = document.forms[0].elements[6].value
+    let dato5 = document.forms[0].elements[7].value
+
+    if ((dato1 && dato2 && dato3 && dato4 &&dato5) != ""){
+        alert(dato1 +"-"+ dato2 + "Se ha registrado con exito") 
+    } 
+}
