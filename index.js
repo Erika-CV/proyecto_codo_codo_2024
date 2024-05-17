d47018079c7c6910542114bc2012898f1d975c13
Erika-CV <erikacamposdeveloper@gmail.com>
2024-05-16 23:20:52 -0300

    modificacion de formulario y Nuestro equipo

--- index.js
+++ index.js
@@ -16,10 +16,12 @@ var validacion = () => {
     let dato1 = document.forms[0].elements[0].value 
     let dato2 = document.forms[0].elements[1].value 
     let dato3 = document.forms[0].elements[2].value
-    let dato4 = document.forms[0].elements[6].value
-    let dato5 = document.forms[0].elements[7].value
+    let dato4 = document.forms[0].elements[3].value
+    let dato5 = document.forms[0].elements[9].value
+    let dato6 = document.forms[0].elements[10].value
+    
 
-    if ((dato1 && dato2 && dato3 && dato4 &&dato5) != ""){
-        alert(dato1 +"-"+ dato2 + "Se ha registrado con exito") 
+    if ((dato1 && dato2 && dato3 && dato4 &&dato5 &&dato6 ) != ""){
+        alert(dato1  +"-"+ dato2  +  " Se ha registrado con exito") 
     } 
 }
